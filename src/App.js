9797750eaf97330ace582dec018c6867f312dcaf
Adejare <inioluwaadesida@gmail.com>
2022-04-15 11:21:07 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from './components/Home';
-import Search from './components/Search'
-import Trendsticker from './components/Trendsticker';
-import Gifcard from './components/Gifcard';
-import SearchResults from './components/SearchResults';
 
 function App() {
   return (
    <Router>
-      <Home/>
-     <Switch>
-       <Route exact path="/"> 
-         <Gifcard />
-       </Route>
-       <Route exact path = "/sticker">
-         <Trendsticker />
-       </Route>
-       <Route exact path="/results">
-         <SearchResults />
-       </Route>
-     </Switch>
+  
    </Router>
   );
 }
