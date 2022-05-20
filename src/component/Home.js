8a09b250c7007a87258864c469e84f3d41488e3f
Adejare <inioluwaadesida@gmail.com>
2022-05-20 21:36:56 +0100

    final test

--- src/component/Home.js
+++ src/component/Home.js
@@ -108,8 +108,8 @@ const Home = () => {
         <>
             {Loading ? (
                 <div className="">
-                    <header className=" w-screen flex md:-row justify-between p-12">
-                        <div className="font-light text-xl">The.weather</div>
+                    <header className="text-center w-screen flex flex-col align-center justify-center md:flex-row md:justify-between p-12">
+                        <div className="font-light text-xl md:mb-0 mb-4">The.weather</div>
                         <div className="text-xl hidden md:flex flex-row">
                             <MdOutlineLocationOn />
                             {response.name}, {response.sys.country}
@@ -127,41 +127,46 @@ const Home = () => {
                         </div>
                     </header>
 
-                    <div className="weatherCard mt-0 mx-auto rounded-3xl shadow flex flex-col justify-around">
-                        <div className="mx-auto mt-14 text-6xl">
+                    <div className="weatherCard mt-0 mb-4 mx-auto rounded-3xl shadow flex flex-col justify-around">
+                        <div className="mx-auto mt-4 md:mt-12 text-6xl">
                             <h1>{response.main.temp}</h1>
                         </div>
                         <div>{/* <img src={} alt="line" /> */}</div>
-                        <div className="image flex align-center justify-around">
+                        <div className="image m-0 flex align-center justify-around">
                             <img
                                 src={`http://openweathermap.org/img/wn/${response.weather[0].icon}@4x.png`}
                             />
                         </div>
-                        <div className="text-center mb-12">
+                        <div className="text-center">
                             <h3>{response.weather[0].main}</h3>
+                            <div className="text-xl text-white text-center flex flex-row align-center justify-center">
+                            <MdOutlineLocationOn />
+                            {response.name}, {response.sys.country}
                         </div>
+                        </div>
+                        
                     </div>
 
                     {/* bottom */}
 
-                    <div className="flex flex-row justify-between my-8 text-center">
+                    <div className="flex flex-row justify-between mt-8 text-center">
                         <div className="mx-12">
                             <h2>Pressure</h2>
                             <p>{response.main.pressure}</p>
                         </div>
-                        <div>
+                        <div className="md:block hidden">
                             <h2>Wind</h2>
                             <p>{response.wind.speed}m/s</p>
                         </div>
 
                         <div className="dateCard w-screen m-auto text-center rounded-xl flex flex-row justifyy-between ">
-                            <div>
+                          
                                 <h2>{todayDate}</h2>
-                            </div>
+                         
                             <p>{month}</p>
                         </div>
 
-                        <div>
+                        <div className="md:block hidden">
                             <h2 >Humidity</h2>
                             <p>{response.main.humidity}</p>
                         </div>
