a87685fc1a0a0d9dd0d8a92b8e1f115783d55381
Adejare <inioluwaadesida@gmail.com>
2022-05-20 21:19:16 +0100

    first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./main.scss";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { BiSearchAlt } from "react-icons/bi";
+// import line from '../images/llline.svg'
 
 const Home = () => {
     const [Input, setInput] = useState("");
     const [Output, setOutput] = useState("");
-    const [Loading, setLoading] = useState(true);
-    const [sunny, setsunny] = useState(false)
+    const [Loading, setLoading] = useState(false);
+    const [sunny, setsunny] = useState(false);
     const APIkey = "595f035757e3560197028500e5259473";
 
     // getting data first by location
     // useEffect(() => {
+
     if (navigator.geolocation) {
         console.log("true");
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -21,8 +25,8 @@ const Home = () => {
                 .get(url)
                 .then((res) => {
                     localStorage.setItem("data", JSON.stringify(res.data));
-                    setLoading(false)
-                    const imgsrc = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+                    setLoading(true);
+                    const imgsrc = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
                 })
                 .catch((err) => {
                     console.log(err);
@@ -39,6 +43,7 @@ const Home = () => {
             alert("Please enter a value");
         } else {
             setTimeout(() => {
+                let imgsrc;
                 axios
                     .get(
                         `https://api.openweathermap.org/data/2.5/weather?q=${Input}&appid=${APIkey}&units=metric`
@@ -49,21 +54,20 @@ const Home = () => {
                         // LOCAL STORAGE
                         localStorage.setItem("data", JSON.stringify(res.data));
                         console.log(Output);
-                        setLoading(false)
+                        setLoading(true);
                         // const imgsrc = `http://openweathermap.org/img/wn/${res.data.}@2x.png`
-                        const imgsrc = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+                        imgsrc = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
                     })
                     .catch((err) => {
                         console.log(err);
                         if (err == "Network Error") {
-                            console.log("nettt")
+                            console.log("nettt");
                         }
                     });
             }, 1000);
         }
     };
-// CHANGING BACKGROUND IF ITS SUNNY OR CLOUDY
-
+    // CHANGING BACKGROUND IF ITS SUNNY OR CLOUDY
     // getting time and date
     let date = new Date();
     let time =
@@ -76,173 +80,100 @@ const Home = () => {
             : "0" + date.getMinutes());
 
     var today = new Date();
-    var todaydate =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
-    console.log(date);
+    const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    var todaydate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-";
+    var month = monthNames[today.getMonth()];
+    var todayDate = today.getDate();
+    // console.log(date);
     // output 2021-7-9
 
     console.log(time);
 
     const response = JSON.parse(localStorage.getItem("data"));
 
-  
-
     return (
         <>
-            {Loading ? 
-                
-                ( <div className={sunny ? "main" : "main cloudy"}>
-                <div className="left">
-                    <div>
-                        <header>the.weather</header>
-                    </div>
-                    <div className="left_mainWeatherInfo">
-                        <div>
-                            <h1>.....</h1>
-                        </div>
-                        <div className="left_locationDate">
-                            <h2>
-                                {Input}, .....
-                            </h2>
-                            <p>
-                                {" "}
-                                {/**date, time */}
-                                {time}, {todaydate}
-                            </p>
-                        </div>
-                        <div className="image">
-                            {/* image */}
-                            {/* <img src={imgsrc} alt="weather image" /> */}
-                            <p>.....</p>
-                        </div>
-                    </div>
-                </div>
-                {/* right */}
-                <div className="right">
-                    <div className="inputform">
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                type="text"
-                                placeholder="Another location"
-                                onChange={(e) => setInput(e.target.value)}
-                                value={Input}
-                            />
-                            <button type="submit">S</button>
-                        </form>
-                    </div>
-                    <div className="locationlist">
-                        <ul>
-                            <li>Birmingham</li>
-                            <li>Manchester</li>
-                            <li>New York</li>
-                            <li>Carlifornia</li>
-                        </ul>
-                    </div>
-                    <hr />
-
-                    {/* weatherdetails */}
-
-                    <div className="weatherdetails">
-                        <h2>Weather Details</h2>
-                        <div>
-                            <p>Pressure</p>
-                            <p>.....</p>
+            {Loading ? (
+                <div className="">
+                    <header className=" w-screen flex flex-row justify-between p-12">
+                        <div className="font-light text-xl">The.weather</div>
+                        <div className="text-xl flex flex-row">
+                            <MdOutlineLocationOn />
+                            {response.name}, {response.sys.country}
                         </div>
                         <div>
-                            <p>Humidity</p>
-                            <p>.....%</p>
-                        </div>
-                        <div>
-                            <p>Wind</p>
-                            <p>.....m/s</p>
-                        </div>
-                        <div>
-                            <p>Clouds</p>
-                            <p>.....</p>
-                        </div>
-                    </div>
-                    <hr />
-                </div>
-            </div>
-        ): ( <div className="main">
-                    <div className="left">
-                        <div>
-                            <header>the.weather</header>
-                        </div>
-                        <div className="left_mainWeatherInfo">
-                            <div>
-                                <h1>{response.main.temp}</h1>
-                            </div>
-                            <div className="left_locationDate">
-                                <h2>
-                                    {response.name}, {response.sys.country}
-                                </h2>
-                                <p>
-                                    {" "}
-                                    {/**date, time */}
-                                    {time}, {todaydate}
-                                </p>
-                            </div>
-                            <div className="image">
-                                {/* image */}
-                                {/* <img src={imgsrc} alt="weather image" /> */}
-                                <p>{response.weather[0].main}</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* right */}
-                    <div className="right">
-                        <div className="inputform">
                             <form onSubmit={handleSubmit}>
                                 <input
                                     type="text"
-                                    placeholder="Another location"
+                                    className="rounded-lg px-2 text-center py-2 focus:ring-1"
+                                    placeholder="Search your Location"
                                     onChange={(e) => setInput(e.target.value)}
                                     value={Input}
                                 />
-                                <button type="submit">S</button>
                             </form>
                         </div>
-                        <div className="locationlist">
-                            <ul>
-                                <li>Birmingham</li>
-                                <li>Manchester</li>
-                                <li>New York</li>
-                                <li>Carlifornia</li>
-                            </ul>
-                        </div>
-                        <hr />
+                    </header>
 
-                        {/* weatherdetails */}
-
-                        <div className="weatherdetails">
-                            <h2>Weather Details</h2>
-                            <div>
-                                <p>Pressure</p>
-                                <p>{response.main.pressure}</p>
-                            </div>
-                            <div>
-                                <p>Humidity</p>
-                                <p>{response.main.humidity}%</p>
-                            </div>
-                            <div>
-                                <p>Wind</p>
-                                <p>{response.wind.speed}m/s</p>
-                            </div>
-                            <div>
-                                <p>Clouds</p>
-                                <p>{response.clouds.all}</p>
-                            </div>
+                    <div className="weatherCard mt-0 mx-auto rounded-3xl shadow flex flex-col justify-around">
+                        <div className="mx-auto mt-14 text-6xl">
+                            <h1>{response.main.temp}</h1>
                         </div>
-                        <hr />
+                        <div>{/* <img src={} alt="line" /> */}</div>
+                        <div className="image flex align-center justify-around">
+                            <img
+                                src={`http://openweathermap.org/img/wn/${response.weather[0].icon}@4x.png`}
+                            />
+                        </div>
+                        <div className="text-center mb-12">
+                            <h3>{response.weather[0].main}</h3>
+                        </div>
+                    </div>
+
+                    {/* bottom */}
+
+                    <div className="flex flex-row justify-between my-8 text-center">
+                        <div className="mx-12">
+                            <h2>Pressure</h2>
+                            <p>{response.main.pressure}</p>
+                        </div>
+                        <div>
+                            <h2>Wind</h2>
+                            <p>{response.wind.speed}m/s</p>
+                        </div>
+
+                        <div className="dateCard w-screen m-auto text-center rounded-xl flex flex-row justifyy-between ">
+                            <div>
+                                <h2>{todayDate}</h2>
+                            </div>
+                            <p>{month}</p>
+                        </div>
+
+                        <div>
+                            <h2 >Humidity</h2>
+                            <p>{response.main.humidity}</p>
+                        </div>
+                        <div className="mx-12">
+                            <h2>Humidity</h2>
+                            <p>{response.main.humidity}</p>
+                        </div>
                     </div>
                 </div>
-            )
-            }
+            ) : (
+                <div>WAIT SMALL</div>
+            )}
         </>
     );
 };
